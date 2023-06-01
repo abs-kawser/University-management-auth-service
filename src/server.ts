b4850be741afa213e4028ab579ea929c23ee625f
Abs kawser <abskawser77000@gmail.com>
2023-06-01 18:03:17 +0600

    testing again

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`Database connection established`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database connection established`)
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
 
-    console.log(`Database connection error: ${config.port}`);
+    console.log(`Database connection error: ${config.port}`)
   }
 }
-bootstrap();
+bootstrap()
+
+//testing server with server configuration settings and testing comment
